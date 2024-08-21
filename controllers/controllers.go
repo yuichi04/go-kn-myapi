9bd53717e3a4cf1ab399775051066b239255b663
yuichi04 <crgone.910428.en@gmail.com>
2024-08-21 09:21:53 +0900

    ハンドラを削除し、コントローラを作成

---
+++ controllers/controllers.go
@@ -0,0 +1,96 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-kn-myapi/models"
+	"go-kn-myapi/services"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type MyAppController struct {
+	service *services.MyAppService
+}
+
+func NewMyAppController(s *services.MyAppService) *MyAppController {
+	return &MyAppController{service: s}
+}
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world\n")
+}
+
+func (c *MyAppController) GetArticleHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query paramter", http.StatusBadRequest)
+		return
+	}
+
+	article, err := c.service.GetArticleService(articleID)
+	if err != nil {
+		http.Error(w, "Fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func (c *MyAppController) GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query paramter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	articleList, err := c.service.GetArticleListService(page)
+	if err != nil {
+		http.Error(w, "Fail to internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(articleList)
+}
+
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "Fail to decode json\n", http.StatusInternalServerError)
+		return
+	}
+
+	newArticle, err := c.service.PostArticleService(reqArticle)
+	if err != nil {
+		http.Error(w, "Fail to internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(newArticle)
+}
+
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "Fail to decode json\n", http.StatusInternalServerError)
+		return
+	}
+
+	newComment, err := c.service.PostCommentServices(reqComment)
+	if err != nil {
+		http.Error(w, "Fail to internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(newComment)
+}
